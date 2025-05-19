43cb485a517d7afcf27aad9e0022f26f19cac155
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-05-19 11:17:30 +0800

    Create domain.js

---
+++ domain.js
@@ -0,0 +1,8 @@
+[
+  { "domain": "baozong.dpdns.org", "registrationDate": "2025-04-25", "expirationDate": "2026-04-25", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
+  { "domain": "townmarshal.dpdns.org", "registrationDate": "2025-04-25", "expirationDate": "2026-04-25", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
+  { "domain": "town95.dpdns.org", "registrationDate": "2025-04-25", "expirationDate": "2026-04-25", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
+  { "domain": "tangshuai.dpdns.org", "registrationDate": "2025-04-27", "expirationDate": "2026-04-27", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
+  { "domain": "town.xx.kg", "registrationDate": "2025-05-05", "expirationDate": "2026-05-05", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
+  { "domain": "tangshuai.xx.kg", "registrationDate": "2025-05-06", "expirationDate": "2026-05-06", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" }
+]
