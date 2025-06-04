13915fb37420b8337590ce97c298aac436748264
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-06-04 09:47:09 +0800

    Update domain.js

--- domain.js
+++ domain.js
@@ -5,5 +5,6 @@
   { "domain": "tangshuai.dpdns.org", "registrationDate": "2025-04-27", "expirationDate": "2026-04-27", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "town.xx.kg", "registrationDate": "2025-05-05", "expirationDate": "2026-05-05", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "tangshuai.xx.kg", "registrationDate": "2025-05-06", "expirationDate": "2026-05-06", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
-    { "domain": "yust.eu.org", "registrationDate": "2025-05-31", "expirationDate": "2030-05-31", "system": "eu.org", "systemURL": "https://nic.eu.org" }
+  { "domain": "yust.eu.org", "registrationDate": "2023-05-21", "expirationDate": "2030-05-21", "system": "eu.org", "systemURL": "https://nic.eu.org" },
+  { "domain": "yutk.eu.org", "registrationDate": "2023-06-01", "expirationDate": "2030-06-011", "system": "eu.org", "systemURL": "https://nic.eu.org" }
 ]
