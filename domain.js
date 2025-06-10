81ab1dc570bbab88f06fa5f35722d8361616d3cc
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-06-10 15:08:49 +0800

    Update domain.js

--- domain.js
+++ domain.js
@@ -7,11 +7,11 @@
   { "domain": "tangshuai.xx.kg", "registrationDate": "2025-05-06", "expirationDate": "2026-05-06", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "yust.eu.org", "registrationDate": "2023-05-21", "expirationDate": "2030-05-21", "system": "eu.org", "systemURL": "https://nic.eu.org" },
   { "domain": "yutk.eu.org", "registrationDate": "2023-06-01", "expirationDate": "2030-06-01", "system": "eu.org", "systemURL": "https://nic.eu.org" },
-    { "domain": "美国Webfreecloud", "registrationDate": "2025-06-10", "expirationDate": "2025-07-12", "system": "eu.org", "systemURL": "123" },
-    { "domain": "德国Webfreecloud", "registrationDate": "2025-06-10", "expirationDate": "2025-07-12", "system": "eu.org", "systemURL": "123" },
-   { "domain": "新加坡Webfreecloud", "registrationDate": "2025-06-10", "expirationDate": "2025-06-15", "system": "eu.org", "systemURL": "123" },
-   { "domain": "荷兰Webfreecloud", "registrationDate": "2025-06-10", "expirationDate": "2025-07-05", "system": "eu.org", "systemURL": "123" },
-     { "domain": "英国Webfreecloud", "registrationDate": "2025-06-10", "expirationDate": "2025-07-05", "system": "eu.org", "systemURL": "123" }
+    { "domain": "美国Webfreecloud", "registrationDate": "2025-05-10", "expirationDate": "2025-07-12", "system": "eu.org", "systemURL": "https://web.freecloud.ltd/" },
+    { "domain": "德国Webfreecloud", "registrationDate": "2025-05-10", "expirationDate": "2025-07-12", "system": "eu.org", "systemURL": "https://web.freecloud.ltd/" },
+   { "domain": "新加坡Webfreecloud", "registrationDate": "2025-05-10", "expirationDate": "2025-06-15", "system": "eu.org", "systemURL": "https://web.freecloud.ltd/" },
+   { "domain": "荷兰Webfreecloud", "registrationDate": "2025-05-10", "expirationDate": "2025-07-05", "system": "eu.org", "systemURL": "https://web.freecloud.ltd/" },
+     { "domain": "英国Webfreecloud", "registrationDate": "2025-05-10", "expirationDate": "2025-07-05", "system": "eu.org", "systemURL": "https://web.freecloud.ltd/" }
 
     
   
