3fd5039405103081e4ed9835af63548162577cd9
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-06-10 15:12:23 +0800

    Update domain.js

--- domain.js
+++ domain.js
@@ -6,12 +6,8 @@
   { "domain": "town.xx.kg", "registrationDate": "2025-05-05", "expirationDate": "2026-05-05", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "tangshuai.xx.kg", "registrationDate": "2025-05-06", "expirationDate": "2026-05-06", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "yust.eu.org", "registrationDate": "2023-05-21", "expirationDate": "2030-05-21", "system": "eu.org", "systemURL": "https://nic.eu.org" },
-  { "domain": "yutk.eu.org", "registrationDate": "2023-06-01", "expirationDate": "2030-06-01", "system": "eu.org", "systemURL": "https://nic.eu.org" },
-    { "domain": "wfusa.baozong.dpdns.org", "registrationDate": "2025-05-10", "expirationDate": "2025-07-12", "system": "eu.org", "systemURL": "https://nic.eu.org" },
-    { "domain": "wfde.baozong.dpdns.org", "registrationDate": "2025-05-10", "expirationDate": "2025-07-12", "system": "eu.org", "systemURL": "https://nic.eu.org" },
-   { "domain": "wfsg.baozong.dpdns.org", "registrationDate": "2025-05-10", "expirationDate": "2025-06-15", "system": "eu.org", "systemURL": "https://nic.eu.org" },
-   { "domain": "helan.yutk.eu.org", "registrationDate": "2025-05-10", "expirationDate": "2025-07-05", "system": "eu.org", "systemURL": "https://nic.eu.org" },
-     { "domain": "english.yutk.eu.org", "registrationDate": "2025-05-10", "expirationDate": "2025-07-05", "system": "eu.org", "systemURL": "https://nic.eu.org" }
+  { "domain": "yutk.eu.org", "registrationDate": "2023-06-01", "expirationDate": "2030-06-01", "system": "eu.org", "systemURL": "https://nic.eu.org" }
+ 
 
     
   
