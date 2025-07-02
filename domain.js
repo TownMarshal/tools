df5943bca2570d3a078b8b1544e7b0b11bef74b9
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-07-02 20:30:20 +0800

    Update domain.js

--- domain.js
+++ domain.js
@@ -5,7 +5,9 @@
   { "domain": "tangshuai.dpdns.org", "registrationDate": "2025-04-27", "expirationDate": "2026-04-27", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "town.xx.kg", "registrationDate": "2025-05-05", "expirationDate": "2026-05-05", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "tangshuai.xx.kg", "registrationDate": "2025-05-06", "expirationDate": "2026-05-06", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
-  { "domain": "yust.eu.org", "registrationDate": "2023-05-21", "expirationDate": "2030-05-21", "system": "eu.org", "systemURL": "https://nic.eu.org" }
+  { "domain": "yust.eu.org", "registrationDate": "2023-05-21", "expirationDate": "2030-05-21", "system": "eu.org", "systemURL": "https://nic.eu.org" },
+  { "domain": "bao.hidns.co", "registrationDate": "2025-07-01", "expirationDate": "2099-12-31", "system": "hidoha.net", "systemURL": "https://www.hidoha.net/" },
+  { "domain": "bao.hidns.vip", "registrationDate": "2025-07-01", "expirationDate": "2099-12-31", "system": "hidoha.net", "systemURL": "https://www.hidoha.net/" }
  
 
     
