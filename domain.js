e1aac82283831018df45ccfec909e1a0bd2fbe46
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-11-29 09:09:12 +0800

    Remove unused domain entries from domain.js
    Removed several domain entries from the list.

--- domain.js
+++ domain.js
@@ -2,16 +2,12 @@
 
   { "domain": "baozong.ip-ddns.com", "registrationDate": "2025-04-28", "expirationDate": "2099-12-31", "system": "cloudns", "systemURL": "https://www.cloudns.net/" },
   { "domain": "baozong.dpdns.org", "registrationDate": "2025-04-25", "expirationDate": "2026-04-25", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
-
   { "domain": "town95.dpdns.org", "registrationDate": "2025-04-25", "expirationDate": "2026-04-25", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
   { "domain": "town.xx.kg", "registrationDate": "2025-05-05", "expirationDate": "2026-05-05", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
-
   { "domain": "an.cloudns.pro", "registrationDate": "2025-08-24", "expirationDate": "2099-12-31", "system": "cloudns", "systemURL": "https://www.cloudns.net/" },  
   { "domain": "bao.cloudns.pro", "registrationDate": "2025-08-24", "expirationDate": "2099-12-31", "system": "cloudns", "systemURL": "https://www.cloudns.net/" },  
-
   { "domain": "bao.hidns.co", "registrationDate": "2025-07-01", "expirationDate": "2099-12-31", "system": "hidoha.net", "systemURL": "https://www.hidoha.net/" },
   { "domain": "bao.hidns.vip", "registrationDate": "2025-07-01", "expirationDate": "2099-12-31", "system": "hidoha.net", "systemURL": "https://www.hidoha.net/" },
-  
   { "domain": "town.netlib.re", "registrationDate": "2025-07-01", "expirationDate": "2099-12-31", "system": ".netlib.re", "systemURL": "https://www.netlib.re/" },
   { "domain": "yust.eu.org", "registrationDate": "2023-05-21", "expirationDate": "2030-05-21", "system": "eu.org", "systemURL": "https://nic.eu.org" },
   { "domain": "towt.nyc.mn", "registrationDate": "2025-11-28", "expirationDate": "2035-11-28", "system": "publiczone.org", "systemURL": "http://publiczone.org/" }
