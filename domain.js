37b14b2ca59585f5ccab8f0501a20bc91489ee34
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2026-04-12 18:56:25 +0800

    Update domain.js

--- domain.js
+++ domain.js
@@ -1,10 +1,4 @@
 [
   { "domain": "baozong.ip-ddns.com", "registrationDate": "2025-04-28", "expirationDate": "2099-12-31", "system": "cloudns", "systemURL": "https://www.cloudns.net/" },
-  { "domain": "baozong.dpdns.org", "registrationDate": "2025-04-25", "expirationDate": "2026-04-25", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
-  { "domain": "town.xx.kg", "registrationDate": "2025-05-06", "expirationDate": "2027-05-06", "system": "DigitalPlat", "systemURL": "https://dash.domain.digitalplat.org" },
-  { "domain": "an.cloudns.pro", "registrationDate": "2025-08-24", "expirationDate": "2099-12-31", "system": "cloudns", "systemURL": "https://www.cloudns.net/" },  
-  { "domain": "bao.cloudns.pro", "registrationDate": "2025-08-24", "expirationDate": "2099-12-31", "system": "cloudns", "systemURL": "https://www.cloudns.net/" },  
-  { "domain": "bao.hidns.co", "registrationDate": "2025-07-01", "expirationDate": "2099-12-31", "system": "hidoha.net", "systemURL": "https://www.hidoha.net/" },
-  { "domain": "bao.hidns.vip", "registrationDate": "2025-07-01", "expirationDate": "2099-12-31", "system": "hidoha.net", "systemURL": "https://www.hidoha.net/" },
   { "domain": "yust.eu.org", "registrationDate": "2023-05-21", "expirationDate": "2030-05-21", "system": "eu.org", "systemURL": "https://nic.eu.org" }
 ]
